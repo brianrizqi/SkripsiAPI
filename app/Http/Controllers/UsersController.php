1b0e9f02a77824e61832673817ee97c299355dc5
brianrizqi <rizqibrian27@gmail.com>
2019-09-22 20:53:30 +0700

    Login and Register API

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use App\Users;
+use Illuminate\Http\Request;
+use Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UsersController extends Controller
+{
+    public function login(Request $request)
+    {
+        $username = $request->username;
+        $password = $request->password;
+        $data = array();
+        if (strpos($username, '@') !== false) {
+            $user = User::where('email', $username)
+                ->first();
+        } else {
+            $user = User::where('username', $username)
+                ->first();
+        }
+        if ($user == null) {
+            $data['error'] = true;
+            $data['message'] = 'Error';
+        }
+
+        if (Auth::attempt(['email' => $username, 'password' => $password]) || Auth::attempt(['username' => $username, 'password' => $password])) {
+            $data['users'] = $user;
+            $data['error'] = false;
+            $data['message'] = 'Success';
+        } else {
+            $data['error'] = true;
+            $data['message'] = 'bzz';
+        }
+        return response()->json($data);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = $this->validator($request->all());
+        $data = array();
+        if ($validator->passes()) {
+            $users = User::register(
+                $request->name,
+                $request->email,
+                $request->username,
+                Hash::make($request->password),
+                $request->land_area
+            );
+            $data['error'] = false;
+            $data['message'] = "Success";
+        } else {
+            $data['error'] = true;
+            $data['message'] = "Something's wrong perhaps a missing item.";
+        }
+        return response()->json($data);
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|min:3|max:50',
+            'username' => 'required|string|min:5|max:20|unique:users',
+            'email' => 'required|string|email|max:191|unique:users',
+            'password' => 'required|string|min:6',
+            'land_area' => 'required',
+        ]);
+    }
+
+}
--- app/User.php
+++ app/User.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'username', 'land_area'
     ];
 
     /**
@@ -36,4 +36,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function register($name, $email, $username, $password, $land_area)
+    {
+        $users = User::create([
+            'name' => $name,
+            'email' => $email,
+            'username' => $username,
+            'password' => $password,
+            'land_area' => $land_area
+        ]);
+        if ($users) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
